20d8105f7a881c8d6e06c301b737d673a596dbe2
Richard Remer <rich.remer@gmail.com>
2023-11-02 09:20:09 -0700

    Repository resource

--- github-api.js
+++ github-api.js
@@ -1,4 +1,5 @@
 export {GitHub} from "./lib/github.js";
+export {Repository} from "./lib/repository.js";
 export {Resource} from "./lib/resource.js";
 export {ResponseError} from "./lib/error.js";
 export {User} from "./lib/user.js";
